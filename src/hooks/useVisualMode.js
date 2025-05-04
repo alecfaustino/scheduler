7caa840bd48b42e6eb57c307d6443e3f7ab10843
Alec Faustino <alecfaustino@outlook.com>
2025-05-04 12:46:51 -0600

    implement conditional to back if already at first, and replace arg in transition

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,12 +2,22 @@ import { useState } from "react";
 export default function useVisualMode(initial) {
   const [history, setHistory] = useState([initial]);
 
-  function transition(newMode){
+  function transition(newMode, replace = false){
+    if(replace) {
+      setHistory(prev => [...prev.slice(0, -1), newMode]);
+    } else {
     setHistory(prev => [...prev, newMode]);
+    }
   }
 
   function back() {
-    setHistory(prev => [...prev.slice(0, prev.length - 1)])
+    setHistory(prev => {
+      // if we're already at the initial, don't change it
+      if(prev.length <= 1) {
+        return prev;
+      }
+      return [...prev.slice(0, prev.length - 1)]
+    })
   }
 
   return { mode: history[history.length -1], transition, back };
