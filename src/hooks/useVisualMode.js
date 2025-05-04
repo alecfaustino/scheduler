01c16797aa952423a94ce42cb818139029808f62
Alec Faustino <alecfaustino@outlook.com>
2025-05-03 21:40:53 -0600

    update transition function to setHistory

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,13 +1,17 @@
 import { useState } from "react";
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
   function transition(newMode){
     setMode(newMode)
+    setHistory((prev) => {
+      [...prev, newMode]
+    });
   }
 
   function back() {
-    
+
   }
 
   return { transition, mode };
