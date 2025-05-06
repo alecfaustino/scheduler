edec5f7a9cb55a49c0cbdea28aa48db2dfbac6bf
Alec Faustino <alecfaustino@outlook.com>
2025-05-05 21:40:21 -0600

    add type validation to the interviewers prop passed into InterviewerList

--- src/components/InterviewerList.jsx
+++ src/components/InterviewerList.jsx
@@ -1,9 +1,13 @@
 import "./InterviewerList.scss";
-
+import PropTypes from "prop-types";
 import InterviewerListItem from "./InterviewerListItem";
 
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired,
+};
+
 function InterviewerList(props) {
-  const interviewers = props.interviewers.map(interviewer => {
+  const interviewers = props.interviewers.map((interviewer) => {
     return (
       <InterviewerListItem
         key={interviewer.id}
@@ -11,7 +15,7 @@ function InterviewerList(props) {
         name={interviewer.name}
         avatar={interviewer.avatar}
         selected={interviewer.id === props.value}
-        setInterviewer={event => props.onChange(interviewer.id)}
+        setInterviewer={(event) => props.onChange(interviewer.id)}
       />
     );
   });
