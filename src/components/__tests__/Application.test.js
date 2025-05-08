91fcffc43cfdb1ff8904ee16a0903304b5034c66
Alec Faustino <alecfaustino@outlook.com>
2025-05-08 08:10:41 -0600

    refactor test with async await

---
+++ src/components/__tests__/Application.test.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Application from "../Application";
+
+describe("Application", () => {
+
+  it("defaults to Monday and changes the schedule when a new day is selected", async () => {
+    const { queryByText, getByText,findByText } = render(<Application />);
+  
+    await findByText("Monday");
+  
+    fireEvent.click(getByText("Tuesday"));
+    expect(queryByText("Leopold Silvers")).toBeInTheDocument();
+  });
+})
