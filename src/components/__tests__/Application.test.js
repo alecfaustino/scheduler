7a929de5c74b5960298a7fffc3973abf3d465a10
Alec Faustino <alecfaustino@outlook.com>
2025-05-09 08:11:04 -0600

    load data with async function for the test

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,16 +1,27 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
+import { render, fireEvent, getByText, prettyDOM } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Application from "../Application";
 
 describe("Application", () => {
 
   it("defaults to Monday and changes the schedule when a new day is selected", async () => {
-    const { queryByText, getByText,findByText } = render(<Application />);
+    const { queryByText, findByText, getByText} = render(<Application />);
   
     await findByText("Monday");
   
     fireEvent.click(getByText("Tuesday"));
     expect(queryByText("Leopold Silvers")).toBeInTheDocument();
   });
+
+  it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
+    const { container, findByText, getAllByTestId } = render(<Application />);
+
+    await findByText("Archie Cohen");
+
+    const appointments = getAllByTestId(container, "appointment");
+    console.log(prettyDOM(appointments), "^^^^^^");
+
+
+  })
 })
