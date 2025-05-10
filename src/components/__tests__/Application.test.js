b44343a062790bfbaaef7b981d034e193b2ae55e
Alec Faustino <alecfaustino@outlook.com>
2025-05-10 16:05:29 -0600

    fire events to save an interview

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { render, fireEvent, getByText, prettyDOM } from "@testing-library/react";
+import { render, fireEvent, getByText, prettyDOM, getAllByTestId, getByAltText, getByPlaceholderText} from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Application from "../Application";
 
 describe("Application", () => {
 
   it("defaults to Monday and changes the schedule when a new day is selected", async () => {
-    const { queryByText, findByText, getByText} = render(<Application />);
+    const { queryByText, findByText, getByText } = render(<Application />);
   
     await findByText("Monday");
   
@@ -15,13 +15,20 @@ describe("Application", () => {
   });
 
   it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
-    const { container, findByText, getAllByTestId } = render(<Application />);
+    const { container, findByText } = render(<Application />);
+
 
     await findByText("Archie Cohen");
-
     const appointments = getAllByTestId(container, "appointment");
-    console.log(prettyDOM(appointments), "^^^^^^");
+    const appointment = appointments[0];
+    console.log(prettyDOM(appointment));
 
+    fireEvent.click(getByAltText(appointment, "Add"));
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
 
-  })
+    fireEvent.click(getByText(appointment, "Save"));
+      })
 })
