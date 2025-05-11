b2e5c33a82de2a683da21a6993f03b5e7100ef19
Alec Faustino <alecfaustino@outlook.com>
2025-05-11 12:33:16 -0600

    refactor navigation tests for best practices

--- cypress/e2e/navigation.cy.js
+++ cypress/e2e/navigation.cy.js
@@ -2,9 +2,9 @@ describe("Navigation", () => {
   it("should Navigate to Tuesday", () => {
     cy.visit("/");
 
-    cy.contains("li", "Tuesday")
+    cy.contains("[data-testid=day]", "Tuesday")
       .click()
-      .should("have.css", "background-color", "rgb(242, 242, 242)");
+      .should('have.class', 'day-list__item--selected');
   });
 
 });
